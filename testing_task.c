8dfd89c5f887e6dd69e94c6c6d8e55af7ca413ca
polina-kononova <98398064+polina-kononova@users.noreply.github.com>
2022-02-17 22:35:52 +0700

    create array

--- testing_task.c
+++ testing_task.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 10
 
-void create_massiv(size_t N, ) {
-  int size_massiv;
-     massiv = (double **)calloc(N, sizeof(double *));
-    for (size_t i = 0; i < N; i++) {
-      size_massiv = rand();
-      m.matrix[i] = (double *)calloc(size_massiv, sizeof(double));
-    }
+void create_arr(int** array, int * size);
+void sort_arr(int** array, int * size);
+void remove_arr(int** array);
+
+void create_arr(int** array, int size[]) {
+  array = (int **)calloc(N, sizeof(int *));
+  for (size_t i = 0; i < N; i++) {
+    size[i] = rand() % 10;
+    array[i] = (int *)calloc(size[i], sizeof(int));
+    printf("array %ld size %d: ", i, size[i]);
+    for (size_t j = 0; j < size[i]; j++) {
+       array[i][j] = rand() % 5;
+       printf("%d ", array[i][j]);
+     }
+     printf("\n");
   }
-  return m;
 }
 
-int main () {
-    double** massiv;
+void sort_arr(int** array, int * size);
+
+void remove_arr(int** array) {
+  for (size_t i = 0; i < N; i++) {
+    free(array[i]);
+  }
+}
 
 
-
+void main () {
+  int** array;
+  int * size = (int *)calloc(N, sizeof(int));
+  create_arr(array, size);
+  // remove_arr(array);
+  free(size);
 }
 
