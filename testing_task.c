3cccec8e50c2ab21f6b2cb26efaadf999bed1da2
polina-kononova <98398064+polina-kononova@users.noreply.github.com>
2022-02-17 21:21:14 +0700

    Create testing_task.c

---
+++ testing_task.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void create_massiv(size_t N, ) {
+  int size_massiv;
+     massiv = (double **)calloc(N, sizeof(double *));
+    for (size_t i = 0; i < N; i++) {
+      size_massiv = rand();
+      m.matrix[i] = (double *)calloc(size_massiv, sizeof(double));
+    }
+  }
+  return m;
+}
+
+int main () {
+    double** massiv;
+
+
+
+}
+
